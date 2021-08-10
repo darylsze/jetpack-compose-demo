aec56aa4a822ec59fc94455d8b485612d270800e
darylsze <sze.kf1004@gmail.com>
2021-08-10 18:23:09 +0800

    feat: with bordered message

--- app/src/main/java/com/example/compose/ui/components/conversation.kt
+++ app/src/main/java/com/example/compose/ui/components/conversation.kt
@@ -1,27 +1,26 @@
 package com.example.compose.ui.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.example.compose.R
+import com.example.compose.ui.theme.ComposeTheme
 
 data class Sender(
     val name: String,
@@ -33,7 +32,7 @@ data class Message(
     val content: String
 )
 
-@Preview
+@Preview(uiMode = UI_MODE_NIGHT_NO)
 @Composable
 fun Conversation() {
     val messages = sequenceOf(
@@ -77,34 +76,46 @@ fun Conversation() {
 
 @Composable
 fun MessageCard(message: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Contact profile",
-            modifier = Modifier
-                .size(35.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(3.dp))
+    ComposeTheme(false) {
         // We keep track if the message is expanded or not in this
         // variable
         var isExpanded by remember {
             mutableStateOf(false)
         }
 
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded } ) {
-            Text(
-                text = message.content,
-                modifier = Modifier.padding(all = 4.dp),
-                // If the message is expanded, we display all its content
-                // otherwise we only display the first line
-                maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                style = MaterialTheme.typography.body2
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "Contact profile",
+                modifier = Modifier
+                    .size(35.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
             )
+            Spacer(modifier = Modifier.width(3.dp))
+            Column(
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded }
+                    .padding(5.dp)
+                    .background(androidx.compose.ui.graphics.Color.White)
+                    .clip(RoundedCornerShape(10))
+            ) {
+                Text(
+                    text = message.sender.name,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold)
+                )
+                Text(
+                    text = message.content,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
-
 }
 
 //@Preview(uiMode = UI_MODE_NIGHT_NO, showBackground = true, name = "LIGHT MODE")
